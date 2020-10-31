05bb4ff358f5867fa3347db3b3b602964ff22d16
Jhobean <fredericgagnon86@hotmail.com>
2020-10-30 23:07:16 -0400

    First commit Ok

---
+++ SphereConvertionUtil/ConsoleSpiner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SphereConvertionUtil
+{
+    public class ConsoleSpiner
+    {
+        int counter;
+        public ConsoleSpiner()
+        {
+            counter = 0;
+        }
+        public void Turn()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("|"); break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+    }
+}
