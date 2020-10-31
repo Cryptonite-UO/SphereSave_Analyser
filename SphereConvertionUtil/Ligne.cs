eb18f5e68b8dd1afc85baf464a524743cdc50545
Jhobean <fredericgagnon86@hotmail.com>
2020-10-30 22:59:58 -0400

    First Commit

---
+++ SphereConvertionUtil/Ligne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SphereConvertionUtil
+{
+    public class Ligne
+    {
+
+        public Ligne(string txt, bool isNewLine)
+        {
+            Text = txt;
+            IsNewLine = isNewLine;
+        }
+
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public bool IsNewLine
+        {
+            get;
+            set;
+        }
+    }
+}
