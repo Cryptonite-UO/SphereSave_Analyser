5505ba49f7e47b1d145d896d731488d142eccfd0
Nirad <miljours@gmail.com>
2020-10-31 21:57:51 -0400

    Ajout de la config dans ingnore

--- SphereSave_Analyser/Program.cs
+++ SphereSave_Analyser/Program.cs
@@ -22,41 +22,40 @@ namespace SphereSave_Analyser
             SphereFileReader reader = new SphereFileReader();
             try
             {
-                reader.ReadFileToObj(dirpathsave+ "/sphereworld.scp", SphereFileType.SphereWorld);
-
-                var gold = from obj in reader.WorldItems
-                           where obj.id == "i_gold"
-                           select obj;
-
-                int amount = 1;
-                foreach (var o in gold)
-                {
-                    if (!String.IsNullOrEmpty(o.amount))
-                        amount += int.Parse(o.amount);
-                }
-
-                Console.WriteLine("Il y yas {0} gold en jeu", amount);
-
-                var query = from item in reader.WorldItems
-                            group item.id by item.id into g
-                            let count = g.Count()
-                            orderby count descending
-                            select new { Id = g.Key, Nombre = count };
-
-                foreach (var x in query)
-                {
-                    if (x.Nombre > 100)
-                    {
-                        Console.WriteLine("id: " + x.Id + " Count: " + x.Nombre);
-                    }
-                }
-
+                reader.ReadFileToObj(dirpathsave + "/sphereworld.scp", SphereFileType.SphereWorld);
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exeption : {0}",e);
             }
+            //Faire des demande dans la liste d'object
 
+            var gold = from obj in reader.WorldItems
+                       where obj.id == "i_gold"
+                       select obj;
+
+            int amount = 1;
+            foreach (var o in gold)
+            {
+                if (!String.IsNullOrEmpty(o.amount))
+                    amount += int.Parse(o.amount);
+            }
+
+            Console.WriteLine("Il y yas {0} gold en jeu", amount);
+
+            var query = from item in reader.WorldItems
+                        group item.id by item.id into g
+                        let count = g.Count()
+                        orderby count descending
+                        select new { Id = g.Key, Nombre = count };
+
+            foreach (var x in query)
+            {
+                if (x.Nombre > 100)
+                {
+                    Console.WriteLine("id: " + x.Id + " Count: " + x.Nombre);
+                }
+            }
             Console.ReadLine();
         }
     }
