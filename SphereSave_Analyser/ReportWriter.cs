55cbbb7d4cb7f2e88aeb28f2e8fc42a64298c14a
Jhobean <fredericgagnon86@hotmail.com>
2020-11-01 20:13:44 -0500

    Writer class

--- SphereSave_Analyser/ReportWriter.cs
+++ SphereSave_Analyser/ReportWriter.cs
@@ -1,11 +1,44 @@
+using System;
 using System.Configuration;
+using System.IO;
 
 namespace SphereSave_Analyser
 {
-    public static class Report
+    //REPORT CLASS permit to write on a file
+    //EX:
+    //      String Nameoffile = DateTime.Now.ToString("yyy.MM.dd") + " Report of item";
+    //      Report.Createfile(Nameoffile);
+    //        foreach (var x in queryitem)
+    //        {
+    //          Report.Write("id: " + x.Id + " Count: " + x.Nombre,Nameoffile);
+    //        }
+
+       public static class Report
     {
         private static string dirpathreport = ConfigurationManager.AppSettings["dirpathreport"];
-        
+    
+        public static void Createfile(string title)
+            //Create the file and erase the old one with the same name
+        {
+            // Create a string array with head of file
+            string[] head = {"THIS REPORT WAS GENERATE AUTOMATICALLY BY SPHERESAVE_ANALYSER", "********************************" };
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(dirpathreport, $"{title}.txt")))
+            {
+                foreach (string line in head)
+                    outputFile.WriteLine(line);
+
+            }
+        }
+
+        public static void Write(string s,string title)
+            //Add a line to an existing file
+        {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(dirpathreport, $"{title}.txt"), true))
+            {
+                outputFile.WriteLine($"{s}");
+            }
+        }
 
     }
 }
